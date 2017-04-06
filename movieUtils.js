961dbc8fc5ff4126cbb658f24b20517c9537b58c
ygol982@gmail.com <yhnYHN123!@#>
2017-04-07 01:04:44 +0300

    video extensions list

--- movieUtils.js
+++ movieUtils.js
@@ -1,8 +1,11 @@
-var globby = require('globby'),
+var videoExtensions = require("./video-extensions.json"),
+	globby = require('globby'),
 	path = require('path'),
 	tnp = require('torrent-name-parser');
 	
 module.exports = {
+	DEFAULT_GLOB: [`**/*.{${videoExtensions.join(',')}}`, '!**/*{sample,Sample,rarbg.com,RARBG.com}*.*'],
+
 	moviesList: function(path, cb){
 		globby(this.DEFAULT_GLOB,  {'cwd': path}).then(movies => {
 			cb(movies.map(tnp));
