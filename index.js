4b4fde5bc3d6d2e0d95cc119e1cfbba0d0f56ba4
ygol982@gmail.com <yhnYHN123!@#>
2017-04-11 13:13:01 +0300

    Stream videos file with ffmpeg

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express'),
 	movieUtils = require('./movieUtils.js'),
 	appConfig = require('./config/appConfig.json'),
+	ffmpeg = require('fluent-ffmpeg'),
+	path = require('path'),
 	api = require('./api.js');
 
 var app = express();
@@ -12,6 +14,44 @@ app.get('/movies_list', (req, res) => {
 	);
 });
 
+app.get('/video/:title', function(req, res) {
+
+	//Get movie path from the local database 
+	let videoPath = movieUtils.titleToPath(req.params.title);
+
+	if(!videoPath) //Movie title does not exists in the database
+		return api.response(res, false, "Movie does not exists", null);
+
+	ffmpeg.ffprobe(path.resolve(appConfig.moviesDir, videoPath.path),function(err, metadata) {
+		//Stream it!
+		res.contentType('flv');
+
+		var pathToMovie = path.resolve(appConfig.moviesDir, videoPath.path);
+
+		var proc = ffmpeg(pathToMovie)
+			.format('flv')
+		    //.flvmeta()
+		    //.size('1:1')
+		    //.videoBitrate('1024k')
+		    .videoCodec('libx264')
+		    //.fps(24)
+		    //.audioBitrate('96k')
+		    .audioCodec('aac')
+		    //.audioFrequency(22050)
+		    //.audioChannels(2)
+			.on('end', function() {
+				console.log('file has been converted succesfully');
+			})
+			.on('error', function(err) {
+				console.log('an error happened: ' + err.message);
+			})
+			.pipe(res, {end:true});
+
+	  	console.log();
+	});
+});
+
 app.listen(appConfig.serverPort, function () {
   console.log("Running!")
 });
+
--- movieUtils.js
+++ movieUtils.js
@@ -1,14 +1,42 @@
-var videoExtensions = require("./video-extensions.json"),
+var dbConfig = require('./config/databaseConfig.json'),
+	videoExtensions = require("./video-extensions.json"),
 	globby = require('globby'),
 	path = require('path'),
-	tnp = require('torrent-name-parser');
+	tnp = require('torrent-name-parser'),
+	low = require('lowdb');
+
+const db = low(dbConfig.databasesDir + '/titleToPath.json');
+
+db.defaults({ videos: [] })
+  .write();
 	
 module.exports = {
-	DEFAULT_GLOB: [`**/*.{${videoExtensions.join(',')}}`, '!**/*{sample,Sample,rarbg.com,RARBG.com}*.*'],
+	videoGlob: [`**/*.{${videoExtensions.join(',')}}`, '!**/*{sample,Sample,rarbg.com,RARBG.com}*.*'],
 
-	moviesList: function(path, cb){
-		globby(this.DEFAULT_GLOB,  {'cwd': path}).then(movies => {
-			cb(movies.map(tnp));
+	titleToPath: function(title){
+		return movieTitle = db.get(dbConfig.titleToPathDb.schemaName)
+		  .find({ title: title })
+		  .value();
+	},
+
+	moviesList: function(moviesDirectory, cb){
+		globby(this.videoGlob,  {'cwd': moviesDirectory}).then(movies => {
+			cb(movies.map(moviePath => {
+				let movieDetails = tnp(path.basename(moviePath));
+
+				//Push movie to the databse
+				db.get(dbConfig.titleToPathDb.schemaName)
+				  .push({ title: movieDetails.title, path: moviePath})
+				  .write()
+
+				movieDetails.path = moviePath;
+
+				return movieDetails;
+			}));
 		});
+	},
+
+	stream: function(movieTitle){
+
 	}
 }
